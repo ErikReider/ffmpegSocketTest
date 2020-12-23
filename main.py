c953f18cff67f1fae692368e7f62d3bf3387bb83
Erik Reider <35975961+erikreider@users.noreply.github.com>
2020-12-23 20:19:54 +0100

    Fixed the memory leak and added comments

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ import cv2
 width, height = 1920, 1080
 timesValue = width * height * 3
 
+# Create the socket and only listen to 1 connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 6969)
+server_address = ("localhost", 6969)
 sock.bind(server_address)
 sock.listen(1)
 
 thread = threading.Thread(target=lambda x: sp.Popen("./" + x, stdin=sp.DEVNULL,
                                                     stderr=sp.DEVNULL, stdout=sp.DEVNULL, bufsize=10**8), args=("send.sh",))
-print('Waiting for a connection')
+print("Waiting for a connection")
 thread.start()
+
+cv2.namedWindow("window", flags=cv2.WINDOW_GUI_NORMAL)
 
 connection, client_address = sock.accept()
 stream = io.BytesIO()
@@ -34,26 +37,21 @@ def close():
 
 atexit.register(close)
 
-try:
-    print('Connection from', client_address)
-    index = 0
-    while True:
-        data = connection.recv(timesValue)
-        if not data:
+print('Connection from', client_address)
+while True:
+    # Get 6220800 bytes of data from the socket (not always that amount)
+    data = connection.recv(timesValue)
+    if not data:
+        break
+    stream.write(data)
+    stream.seek(0, 2)
+    # When the stream index reaches 6220800, show a new frame (Each frame is W * H * 3 (R,G,B))
+    if stream.tell() >= timesValue:
+        in_bytes = stream.getvalue()[0: timesValue]
+        stream = io.BytesIO(stream.getvalue()[timesValue:])
+        if not in_bytes:
             break
-        stream.write(data)
-        stream.seek(0, 2)
-
-        if stream.tell() >= index + timesValue:
-            in_bytes = stream.getvalue()[index: index + timesValue]
-            # print(in_bytes)
-            index += timesValue
-            if not in_bytes:
-                break
-            in_frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
-            cv2.imshow('in_frame', in_frame)
-
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
-finally:
-    exit(0)
+        # Convert the raw bytes to rgb values and show the image
+        cv2.imshow("window", np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3]))
+        # Wait for 1ms to prevent closing
+        cv2.waitKey(1)
