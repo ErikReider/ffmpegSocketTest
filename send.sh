e78dc7a6a1139f3eeb6784aafddaabf4982fc2bd
Erik Reider <35975961+erikreider@users.noreply.github.com>
2020-12-23 21:49:30 +0100

    send.sh now picks the primary monitor

--- send.sh
+++ send.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-ffmpeg -f x11grab -s 1920x1080 -r 30 -i :0+1920,0 -threads 0 -f rawvideo -pix_fmt bgr24 -an -sn tcp://localhost:6969
+IFS=$'\n'
+for monitor in `xrandr --listactivemonitors`; do if [[ "$monitor" == *"+*"* ]]; then break; fi; done
+
+monParse=`echo $monitor | cut -f4 -d' '`
+height=`echo $monParse | cut -f2 -d'/' | cut -f2 -d'x'`
+width=`echo $monParse | cut -f1 -d'/'`
+xOffset=`echo $monParse | cut -f2 -d'+'`
+yOffset=`echo $monParse | cut -f3 -d'+'`
+
+ffmpeg -f x11grab -s "$width"x"$height" -r 30 -i :0+"$xOffset","$yOffset" -threads 0 -f rawvideo -pix_fmt bgr24 -an -sn tcp://localhost:6969
